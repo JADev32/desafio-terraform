f3b24056c4ffdf07147936d26c3a5e7fd2057480
magali <maguimourino@gmail.com>
2025-11-18 23:08:09 -0300

    add use lock file

--- backend.tf
+++ backend.tf
@@ -3,6 +3,7 @@ terraform {
     bucket  = "terraform-state-lab3-607007849260" # 
     key     = "ecs-lab3/terraform.tfstate"
     region  = "us-east-1"
+    use_lockfile = true 
     encrypt = true
   }
 }
